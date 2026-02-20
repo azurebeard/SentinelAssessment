301f8819ab8e6348b5bc89e032de188786eb1573
azurebeard <ben+azurebeard@cloudbenos.co.uk>
2026-02-20 22:40:51 +0000

    Update Invoke-SARun.ps1

--- src/SentinelAssessment/Public/Invoke-SARun.ps1
+++ src/SentinelAssessment/Public/Invoke-SARun.ps1
@@ -1,1 +1,28 @@
+function Invoke-SARun {
+  [CmdletBinding()]
+  param(
+    [Parameter(Mandatory=$true)][string]$SubscriptionId,
+    [Parameter(Mandatory=$true)][string]$ResourceGroupName,
+    [Parameter(Mandatory=$true)][string]$WorkspaceName,
+    [Parameter(Mandatory=$true)][string]$OutDir,
+    [Parameter(Mandatory=$true)][string]$TemplatesDir,
 
+    [ValidateSet("All","Collect","Normalise","Render")]
+    [string]$Steps = "All",
+
+    [int]$DaysIngestionLookback = 30,
+    [int]$DaysHealthLookback = 30
+  )
+
+  if ($Steps -in @("All","Collect")) {
+    Invoke-SACollect -SubscriptionId $SubscriptionId -ResourceGroupName $ResourceGroupName -WorkspaceName $WorkspaceName -OutDir $OutDir -DaysIngestionLookback $DaysIngestionLookback -DaysHealthLookback $DaysHealthLookback
+  }
+
+  if ($Steps -in @("All","Normalise")) {
+    Invoke-SANormalize -OutDir $OutDir
+  }
+
+  if ($Steps -in @("All","Render")) {
+    Invoke-SARender -OutDir $OutDir -TemplatesDir $TemplatesDir
+  }
+}
