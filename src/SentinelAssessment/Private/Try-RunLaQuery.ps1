18d826e44d7e8fd34f77b2f71a94bfdc4154bfcb
azurebeard <ben+azurebeard@cloudbenos.co.uk>
2026-02-20 23:05:34 +0000

    Update Try-RunLaQuery.ps1

--- src/SentinelAssessment/Private/Try-RunLaQuery.ps1
+++ src/SentinelAssessment/Private/Try-RunLaQuery.ps1
@@ -2,12 +2,28 @@ function Try-RunLaQuery {
   param(
     [Parameter(Mandatory=$true)][Guid]$WorkspaceCustomerId,
     [Parameter(Mandatory=$true)][string]$Query,
-    [Parameter(Mandatory=$true)][string]$Timespan
+    [Parameter(Mandatory=$true)][int]$Days
   )
+
   try {
-    $res = Invoke-AzOperationalInsightsQuery -WorkspaceId $WorkspaceCustomerId -Query $Query -Timespan $Timespan
-    return @{ Success=$true; Error=$null; Results=$res.Results }
-  } catch {
-    return @{ Success=$false; Error=$_.Exception.Message; Results=$null }
+    $timeSpan = New-TimeSpan -Days $Days
+
+    $res = Invoke-AzOperationalInsightsQuery `
+      -WorkspaceId $WorkspaceCustomerId `
+      -Query $Query `
+      -Timespan $timeSpan
+
+    return @{
+      Success = $true
+      Error   = $null
+      Results = $res.Results
+    }
+  }
+  catch {
+    return @{
+      Success = $false
+      Error   = $_.Exception.Message
+      Results = $null
+    }
   }
 }
