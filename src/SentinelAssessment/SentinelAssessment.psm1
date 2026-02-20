12c989fa54c1665276b47c225d361284a4c794d7
azurebeard <ben+azurebeard@cloudbenos.co.uk>
2026-02-20 22:39:38 +0000

    Update SentinelAssessment.psm1

--- src/SentinelAssessment/SentinelAssessment.psm1
+++ src/SentinelAssessment/SentinelAssessment.psm1
@@ -1,1 +1,5 @@
+$public  = Join-Path $PSScriptRoot 'Public'
+$private = Join-Path $PSScriptRoot 'Private'
 
+Get-ChildItem -Path $private -Filter *.ps1 -File | ForEach-Object { . $_.FullName }
+Get-ChildItem -Path $public  -Filter *.ps1 -File | ForEach-Object { . $_.FullName }
